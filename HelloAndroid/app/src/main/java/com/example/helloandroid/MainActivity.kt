e290677dc1e7c0f24c2d9b42ae15b8e7e9303c34
Frank McCown <fmccown@harding.edu>
2022-02-02 15:53:56 -0600

    Add project

---
+++ HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.helloandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sayHello(view: View) {
+        // Extract the text from EditText
+        val helloEditText = findViewById<EditText>(R.id.name_edit_text)
+        val name = helloEditText.text
+
+        // Put "Hello " + name in TextView
+        val helloTextView = findViewById<TextView>(R.id.hello_text_view)
+        helloTextView.text = "Hello $name"
+
+        Log.d("test", "This is a test")
+    }
+}
