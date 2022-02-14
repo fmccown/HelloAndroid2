7d04b3a65a569be02c2873feacf181af1a63efdd
Frank McCown <fmccown@harding.edu>
2022-02-14 15:24:02 -0600

    Replace LinearLayout with ConstraintLayout, etc.

--- HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
@@ -12,6 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val helloBtn = findViewById<Button>(R.id.hello_button)
+        helloBtn.setOnClickListener(this::sayHello)
+
+        //val x= 1/0
+
     }
 
     fun sayHello(view: View) {
@@ -19,12 +26,16 @@ class MainActivity : AppCompatActivity() {
         val helloEditText = findViewById<EditText>(R.id.name_edit_text)
         val name = helloEditText.text
 
-        // Put "Hello " + name in TextView
-        val helloTextView = findViewById<TextView>(R.id.hello_text_view)
-        helloTextView.text = "Hello $name"
-
         Log.d("test", "This is a test")
 
-        Toast.makeText(this, "Hello $name", Toast.LENGTH_SHORT).show()
+        // Put "Hello " + name in TextView
+        val helloTextView = findViewById<TextView>(R.id.hello_text_view)
+
+        // Get the string from strings.xml
+        val helloText = getString(R.string.hello_message, name)
+
+        // Put the "Hello NAME" string in the UI
+        helloTextView.text = helloText
+        Toast.makeText(this, helloText, Toast.LENGTH_SHORT).show()
     }
 }
